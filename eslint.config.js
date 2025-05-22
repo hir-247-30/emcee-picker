b4c30e2132e46c183005167a4c83b2689224a53e
ryo-morimoto <86156534+ryo-morimoto@users.noreply.github.com>
2025-05-23 04:04:45 +0900

    chore: vitestとstylisticの導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,74 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import vitest from '@vitest/eslint-plugin';
 
-export default tseslint.config(
-  {
-    files: ['**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
-    rules: {
-      'semi': 'error',
-      'space-before-function-paren': 'error'
+export default defineConfig(
+    globalIgnores(['dist', 'coverage', 'node_modules']),
+    eslint.configs.recommended,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService : true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-    languageOptions: {
-      sourceType: 'module',
+    // 全て
+    {
+        files  : ['**/*.{js,ts,mjs}'],
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            '@stylistic/space-before-function-paren': 'error',
+
+            '@stylistic/semi'       : 'error',
+            '@stylistic/indent'     : ['error', 4],
+            '@stylistic/key-spacing': ['error', {
+                beforeColon: false,
+                afterColon : true,
+                align      : 'colon',
+            }],
+        },
     },
-  },
+    // 設定ファイル・スクリプト
+    {
+        files          : ['*.config.{js,ts,mjs}'],
+        languageOptions: {
+            globals: { process: 'readonly' },
+        },
+    },
+    // アプリケーション
+    {
+        files  : ['src/**/*.ts'],
+        extends: [
+            ...tseslint.configs.strictTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
+        languageOptions: {
+            sourceType: 'module',
+        },
+    },
+    // テストファイル用: VitestグローバルAPI
+    {
+        files  : ['**/*.test.ts'],
+        plugins: {
+            '@vitest': vitest,
+        },
+        rules: {
+            ...vitest.configs.recommended.rules,
+        },
+        settings: {
+            vitest: {
+                typeCheck: true,
+            },
+        },
+        languageOptions: {
+            globals: {
+                ...vitest.environments.env.globals,
+            },
+        },
+    },
 );
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+    plugins: [tsconfigPaths()],
+    test   : {
+        include : ['src/**/*.test.ts'],
+        globals : true,
+        coverage: {
+            include: ['src/**/*.ts'],
+        },
+    },
+});
